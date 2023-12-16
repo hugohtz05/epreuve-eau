ec464d19432366c94354f2fba0ebccb7757be619
hugohtz03 <hugoheintz03@gmail.com>
2023-12-16 19:07:20 +0100

    Combinaisons de 3 chifres

--- eau01.js
+++ eau01.js
@@ -1,14 +1,18 @@
 // #fonctions utilisées 
-function number3() {
+function showTreeNumber() {
     for (let a = 0; a < 10; a++) {
         for (let b = a + 1; b < 10; b++) {
             for (let c = b + 1; c < 10; c++) {
 
-                // résolution
+                // #resolution
+            function combineNumber() {
                 let combination = [a, b, c];
+            }
 
                 // affichage
-                console.log(combination.join(''));
+            function showNumber() {
+                console.log(combination.join(''));  
+            }
             }
         }
     }
