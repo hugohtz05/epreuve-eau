fcf4099f759510ff52966dae1d0d05aac128ad48
hugohtz03 <hugoheintz03@gmail.com>
2023-12-20 09:40:34 +0100

    suite de fibonacci

--- eau04.js
+++ eau04.js
@@ -1,16 +1,40 @@
-function fibonacci() {
-    let argument = parseInt(process.argv[2]); 
-    let counter = 0;
+// Fonction utilisée
+function fibonacciNthElement(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
     let a = 0;
     let b = 1;
-    
-    while (counter < argument) {
+
+    for (let i = 2; i <= n; i++) {
         let c = a + b;
         a = b;
         b = c;
-        counter++;
     }
-    console.log(a);
+
+    return b;
 }
 
-fibonacci();
+// Gestion des erreurs
+
+function isNumber(argument) {
+    if (isNaN(argument) || argument < 0) {
+        console.log("Erreur : l'argument doit être un nombre entier positif");
+        process.exit(1);
+    }
+}
+
+// Parsing
+function getArguments() {
+    let argument = parseInt(process.argv[2]);
+    return argument;
+}
+
+// Résolution
+let argument = getArguments();
+isNumber(argument);  
+ 
+let result = fibonacciNthElement(argument);
+
+// Affichage
+console.log(result);
