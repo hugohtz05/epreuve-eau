529c496068c19865dcb2cc194e976c98bab6a163
hugohtz03 <hugoheintz03@gmail.com>
2023-12-22 11:28:20 +0100

    suite de fibonacci

--- eau03.js
+++ eau03.js
@@ -1,16 +1,40 @@
-	// fonction utilisées
-function reverseArgument() {
-    const argument = process.argv.slice(2);
+// Fonction utilisée
+function fibonacciNthElement(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
-    let result = "";
-    for (let i = argument.length - 1; i >= 0; i--) {
-        result += argument[i];
-        if (i > 0) {
-            result += ' '; 
-        }
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
     }
-    // affichage
-    console.log(typeof(result));
+
+    return b;
 }
 
-reverseArgument();
+// Gestion des erreurs
+
+function isNumber(argument) {
+    if (isNaN(argument) || argument < 0) {
+        console.log("Erreur : l'argument doit être un nombre entier positif");
+        process.exit(1);
+    }
+}
+
+// Parsing
+function getArguments() {
+let argument = parseInt(process.argv[2]);
+    return argument;
+}
+
+// Résolution
+let argument = getArguments();
+isNumber(argument);  
+ 
+let result = fibonacciNthElement(argument);
+
+// Affichage
+console.log(result);
